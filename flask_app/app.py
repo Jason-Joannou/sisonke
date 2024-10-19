72b64c78e2ccd03fdbe59adac6dfbe9a9059bddc
Jason Joannou <jjoannou@bscglobal.com>
2024-10-19 18:16:22 +0200

    flask app

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,15 @@
-from flask
+from flask import Flask
 from flask_cors import CORS  # Import Flask-CORS
+
+from flask_app.routes.whatsapp import whatsapp_bp
 
 app = Flask(__name__)
 
 # Enable CORS for all routes and all origins
 CORS(app)  # Add this lines to enable CORS for the entire app
+
+app.register_blueprint(whatsapp_bp)
+
 
 @app.route("/")
 def index() -> str:
