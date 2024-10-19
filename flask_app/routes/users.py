f1396e2de37fa0ec9ffd862786296e3e6bc0a389
Jason Joannou <jjoannou@bscglobal.com>
2024-10-19 18:25:48 +0200

    users endpoint

---
+++ flask_app/routes/users.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, request
+
+users_bp = Blueprint("whatsapp", __name__)
+
+
+BASE_ROUTE = "/users"
+
+
+@users_bp.route(BASE_ROUTE, methods=["GET"])
+def users() -> str:
+
+    return "USERS API ENDPOINT"
