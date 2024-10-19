354b5add26fbb4726e44a6be0bf628c4702878c1
Jason Joannou <jjoannou@bscglobal.com>
2024-10-19 22:37:35 +0200

    insert user query

--- database/users/queries.py
+++ database/users/queries.py
@@ -20,3 +20,21 @@ def check_if_number_exists_sqlite(from_number: str) -> bool:
         except Exception as e:
             print(f"An error occurred in check_if_number_exists: {e}")
             raise e
+
+
+def insert_user(user_number: str, user_wallet: str, tiger_beetle_id: str) -> None:
+    from_number = extract_whatsapp_number(from_number=user_number)
+    query = "INSERT INTO USERS (user_number, user_wallet, tiger_beetle_id) VALUES (:from_number, :user_wallet, :tiger_beetle_id)"
+    with sqlite_conn.connect() as conn:
+        try:
+            conn.execute(
+                text(query),
+                {
+                    "from_number": from_number,
+                    "user_wallet": user_wallet,
+                    "tiger_beetle_id": tiger_beetle_id,
+                },
+            )
+        except Exception as e:
+            print(f"An error occurred in insert_user: {e}")
+            raise e
